260c4e059d95ebe578b34ae47c998fa3ee8c2073
thibaut@digitalmind.be <thibaut@digitalmind.be>
2024-06-25 21:26:26 +0200

    setup DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Ingredient;
+use App\Models\Recipe;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +16,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        Category::factory(10)->create();
+
+        Ingredient::factory(500)->create();
+
+        Recipe::factory(500)->create()->each(function ($recipe) {
+            $units       = [
+                'kg',
+                'liter',
+                'g',
+                'ml',
+                'tbsp',
+                'tsp',
+            ];
+            $ingredients = Ingredient::inRandomOrder()->take(rand(3, 10))->pluck('id');
+
+            foreach ($ingredients as $ingredient) {
+                $recipe->ingredients()->attach($ingredient, [
+                    'quantity' => rand(1, 100) / 10,
+                    'unit' => $units[array_rand($units)],
+                ]);
+            }
+        });
     }
 }
